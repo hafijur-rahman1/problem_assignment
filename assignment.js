c7ec93b8fe7f1bde65df2ee1e4a4279ab1e091d6
hafijur-rahman1 <hafijur.ingo24@gmail.com>
2020-10-04 23:06:33 +0600

    assignment done

--- assignment.js
+++ assignment.js
@@ -1,3 +1,5 @@
+// feetToMile calculator
+
 function feetToMile(feet) {
     var mile = feet / 5280;
     return mile;
@@ -5,7 +7,7 @@ function feetToMile(feet) {
 var newFeet = feetToMile(1);
 console.log("Your answer is =", newFeet);
 
-// Cubic Feet calculator
+// Cubic Feet calculator**
 
  function woodCalculator (chair, table, bed){
      var total = chair*1 + table*3 + bed*5;
@@ -49,23 +51,3 @@ console.log(newName);
 
 
 
-
-// **********************************
-var friendName = ["php","css","JavaScript","jQuery","C#"];
-
-var tinyName = [];
-for (var i = 0; i < friendName.length; i++)
-{
-tinyName.push(friendName[i].length);
-// Creating a new array with string length of each element of a given array
-}
-
-var x = tinyName.indexOf(Math.max(...tinyName));
-/*
-Math.max(...strLength) - finds max number in new array which presents the longest string
-in the given array.
-x = strLength.indexOf(...) finds the position of the longest string...
-*/
-
-console.log(tinyName[x]); // Gets string with x position of given array
-// console output: JavaScript
