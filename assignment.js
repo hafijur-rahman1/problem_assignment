e84d358b08bb83dfde8227d129ccad49e1aeab64
hafijur-rahman1 <hafijur.ingo24@gmail.com>
2020-10-04 22:59:11 +0600

    all done

--- assignment.js
+++ assignment.js
@@ -29,5 +29,43 @@ function brickCalculator(floor) {
 }
 var myBuilding = brickCalculator(34);
 console.log("You need total", myBuilding, "bricks");
-    
 
+
+// function tinyFriend
+
+var my_arry = ["hero", "musana", "sallu", "elaina","pi"];
+
+function tinyFriend(arry) {
+	return arry.reduce(function(prevWord,currWord) {
+		if (currWord.length < prevWord.length) {
+			return currWord;
+		}
+		else 
+			return prevWord;
+	})
+}
+var newName = tinyFriend(my_arry);
+console.log(newName);
+
+
+
+
+// **********************************
+var friendName = ["php","css","JavaScript","jQuery","C#"];
+
+var tinyName = [];
+for (var i = 0; i < friendName.length; i++)
+{
+tinyName.push(friendName[i].length);
+// Creating a new array with string length of each element of a given array
+}
+
+var x = tinyName.indexOf(Math.max(...tinyName));
+/*
+Math.max(...strLength) - finds max number in new array which presents the longest string
+in the given array.
+x = strLength.indexOf(...) finds the position of the longest string...
+*/
+
+console.log(tinyName[x]); // Gets string with x position of given array
+// console output: JavaScript
